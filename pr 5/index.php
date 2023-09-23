d110ee3440c3568f0e5c9da1de9badb276b8ef7b
Lofimirai <dmitriy.yrkov.v@gmail.com>
2023-09-23 10:33:00 +0500

    Update index.php

--- pr 5/index.php
+++ pr 5/index.php
@@ -1,22 +1,42 @@
 <?php 
 $host = "db4.myarena.ru";
+$dbname = "u19978_b14";
 $username = "u19978_b14";
 $password = "eR9eA8zO2a";
-$dbname = "u19978_b14";
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Устанавливаем режим обработки ошибок PDO
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Подключение успешно";
-} catch(PDOException $e) {
-    echo "Ошибка подключения: " . $e->getMessage();
-}
-$result = $connection->query('SELECT * FROM `TABLE_NAME`');
-while($row = $result->fetch( PDO::FETCH_ASSOC )){
-    foreach ($row as $key => $value) {
-        echo "<td>".$value."</td>";
-    }
-}
+    $connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $username, $password);
 
+    } catch (PDOException $e) {
+        die("Ошибка подключения: " . $e->getMessage());
+    }
+
+    $result = $connection->query('SELECT * FROM  `product` WHERE 1');
+
+    if ($result){
+    if ($result->rowCount() == 0) {
+        echo "Запрос не вернул результатов.";
+    } else {
+
+        echo "<table border='1'>";
+        // Вывод заголовков таблицы
+        echo "<tr>";
+        for ($i = 0; $i < $result->columnCount(); $i++) {
+            $column = $result->getColumnMeta($i);
+            echo "<th>{$column['name']}</th>";
+        }
+        echo "</tr>";
+        
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>";
+            foreach ($row as $value) {
+                echo "<td>".$value."</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+        }}
+        else {
+            echo "Запросвыполнен с ошибкой ";
+        }
 ?>
