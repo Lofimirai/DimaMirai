cefbfecc62f764e9d47e7d3ee5563a1ced95d97b
Lofimirai <dmitriy.yrkov.v@gmail.com>
2023-09-23 11:11:55 +0500

    Update index.php

--- pr 5/index.php
+++ pr 5/index.php
@@ -4,30 +4,35 @@ $dbname = "u19978_b14";
 $username = "u19978_b14";
 $password = "eR9eA8zO2a";
 
-try {
+try 
+{   
     $connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $username, $password);
-
-    } catch (PDOException $e) {
-        die("Ошибка подключения: " . $e->getMessage());
+}   
+catch (PDOException $e) 
+{   
+    die("Ошибка подключения: " . $e->getMessage());
     }
 
     $result = $connection->query('SELECT * FROM  `product` WHERE 1');
 
-    if ($result){
+    if ($result)
+    {
     if ($result->rowCount() == 0) {
         echo "Запрос не вернул результатов.";
-    } else {
-
+    } 
+    else 
+    {
         echo "<table border='1'>";
         // Вывод заголовков таблицы
         echo "<tr>";
-        for ($i = 0; $i < $result->columnCount(); $i++) {
+        for ($i = 0; $i < $result->columnCount(); $i++) 
+        {
             $column = $result->getColumnMeta($i);
             echo "<th>{$column['name']}</th>";
         }
         echo "</tr>";
-        
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) 
+        {
             echo "<tr>";
             foreach ($row as $value) {
                 echo "<td>".$value."</td>";
