01dc329309516625a81927c9d5453decb276daf8
Lofimirai <dmitriy.yrkov.v@gmail.com>
2023-09-30 10:47:04 +0500

    Create index.php

---
+++ pr 6/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+   
+    <form action="action.php" method="post">
+    <p>В данном примере, форма ссылается сама на себя</p>
+    <p>Ваше имя: <input type="text" name="name"></p>
+    <p>Ваш возраст: <input type="text" name="age"></p>
+    <p><input type="submit"></p> 
+    </form>
+
+    
+</body>
+</html>
