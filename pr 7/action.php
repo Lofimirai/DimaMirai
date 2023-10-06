4be5a217db8443001a2b25e0f92b508b3a978560
Lofimirai <dmitriy.yrkov.v@gmail.com>
2023-10-06 09:45:53 +0500

    Update action.php

--- pr 7/action.php
+++ pr 7/action.php
@@ -1,19 +1,33 @@
 <?php 
-        if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit;
-        if(empty($_POST['name'])) exit('Поле "логин" не заполнено');
-        if(empty($_POST['password'])) exit('Поле "пароль" не заполнено');
-        if(empty($_POST['password1'])) exit('Поле "пароль" не заполнено');
+  
+        if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) 
+                exit;
 
+        if(empty($_POST['name']))
+                exit('Поле "логин" не заполнено');
 
-         
-        if($password) !== ($password1 )
-        exit ('пароли не совпадают');
+        if(empty($_POST['password'])) 
+                exit('Поле "пароль" не заполнено');
 
+        if(empty($_POST['password1'])) 
+                exit('Поле "Подверждение пароля" не заполнено');
 
+        if($_POST['password'] !== $_POST['password1']) 
+                exit('пароли не совпадают' );
 
+        if (strlen($_POST['password']) < 9) {
+            
+                exit('Пароль должен быть минимум 8 символов');
+        }
 
-        if(empty($_POST['mail'])) exit('Поле "почта" не заполнено');
-        //if(empty($_POST['password1']))str_land
+        if (strlen($_POST['name']) < 9) {
+           
+                exit('Логин должен быть минимум 8 символов ');
+        }
+        
+        if(empty($_POST['mail'])) 
+                exit('Поле "почта" не заполнено');
+
         echo 'Здравствуйте, '.htmlspecialchars($_POST['name']).'. <br>';
         echo 'Ваш аккаунт создан'.'. <br>';
 
