1397324f4714f7ba70a354687f6127f4929c3436
Lofimirai <dmitriy.yrkov.v@gmail.com>
2023-10-07 11:27:17 +0500

    Create index.php

---
+++ pr 8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>АФТОРИЗУЙСЯ<h3>
+     <p><span class="error"> * Обязательное поле </span></p>
+     
+     <form action="action.php" method="post">
+     <p>Логин:<span class="error">* <input type="text" name="login"></p>     
+     <p>Пароль:<span class="error">* <input type="password" name="password"></p>
+     <p><input type="submit" value="Ну тыкни сюда, Тык...."></p> 
+     </form>
+     
+</body>
+</html>
