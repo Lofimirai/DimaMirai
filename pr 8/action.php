9354b640061834876fea7aa587729447d205fb1d
Lofimirai <dmitriy.yrkov.v@gmail.com>
2023-10-13 09:48:04 +0500

    Update action.php

--- pr 8/action.php
+++ pr 8/action.php
@@ -12,20 +12,25 @@
         $host       = "db4.myarena.ru";      
         $dbname     = "u19978_b14";   
         $user       = "u19978_b14";          
-        $password   = "eR9eA8zO2a";              
+        $password   = "eR9eA8zO2a";   
+                   
         $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
 
         $link = '<a href = "./index.php">На главную</a>';
 
         if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
-            if(empty($_POST['login'])) exit('Не все поля формы заполнены'.'<br>'.$link);
-            if(empty($_POST['password'])) exit('Не все поля формы заполнены'.'<br>'.$link);
+            if(empty($_POST['login'])) 
+            exit('Не все поля формы заполнены'.'<br>'.$link);
+
+            if(empty($_POST['password'])) 
+            exit('Не все поля формы заполнены'.'<br>'.$link);
         }
         
         $userLogin= $_POST['login'];
     
         $stmt = $connection->prepare("SELECT * FROM `user` WHERE `login` = ? or `Email` = ?");
+
         $stmt->execute([$userLogin, $userLogin]);
 
         $row = $stmt->fetch();
@@ -34,15 +39,13 @@
             exit('Пользователь с такими данными не зарегистрирован'.'<br>'.$link);
             die;
         }
-        if ($row && password_verify($_POST['password'],$row ["password"])) exit ("Успешно вошол".$link );
+
+        if ($row && password_verify($_POST['password'],$row ["password"])) 
+        exit ( "Успешно вошол".$link .'<br>');
         
         else{
                 echo "не правильный логин или пароль";
         }
-
-      
-        
-
     ?>
 </body>
 </html>
