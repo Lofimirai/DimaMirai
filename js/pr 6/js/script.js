fc56f2fb6f24582dbb830933c5d15f21d0ece49c
Lofimirai <dmitriy.yrkov.v@gmail.com>
2023-10-28 10:19:16 +0500

    Create script.js

---
+++ js/pr 6/js/script.js
@@ -0,0 +1,9 @@
+let userVariable = prompt("Введите значение переменной a");
+let correctMessage = "верно";
+let wrongMessage = "неверно";
+
+if (userVariable >= 0) {
+    alert(correctMessage);
+} else {
+    alert(wrongMessage);
+}
