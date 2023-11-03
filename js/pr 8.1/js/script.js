9951ecc28c39ff5feb308f165b0785a58e9b17b0
Lofimirai <permsalimanov2020@gmail.com>
2023-11-03 10:10:43 +0500

    Create script.js

---
+++ js/pr 8.1/js/script.js
@@ -0,0 +1,21 @@
+let name = "Ярков Дмитрий Витальевич";
+let dolchnost = "Студент";
+let level = "Среднее";
+let cotegogi = "Первая.";
+let obrozovanie = "ОНИКС";
+let dic = "Информатика.";
+let prim = "Примечание:";
+
+document.write(name);
+document.write('<br>')
+document.write('Должность ' + dolchnost );
+document.write('<br>')
+document.write('Уровень ' + level );
+document.write('<br>')
+document.write('Категория ' + cotegogi );
+document.write('<br>')
+document.write('Организация ' + obrozovanie );
+document.write('<br>')
+document.write('Дисциплина ' + dic );
+document.write('<br>')
+document.write('Примечание ' + prim );
