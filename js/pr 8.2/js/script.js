56d0b6fb5d88d946e2bccff5a3ae9d0add27eca3
Lofimirai <permsalimanov2020@gmail.com>
2023-11-03 10:39:08 +0500

    Create script.js

---
+++ js/pr 8.2/js/script.js
@@ -0,0 +1,15 @@
+let arr=[];
+
+arrp=[
+'sname','name','pname','dolchnost','level','cotegogi','obrozovanie','dis','prim'] = 'Ярков','Дмитрий','Витальевич',
+''
+
+
+
+let name = "Ярков Дмитрий Витальевич";
+let dolchnost = "Студент";
+let level = "Среднее";
+let cotegogi = "Первая.";
+let obrozovanie = "ОНИКС";
+let dis = "Информатика.";
+let prim = "Примечание:";
