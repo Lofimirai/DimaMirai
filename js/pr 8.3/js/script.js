544a797c87d522e3eb520259816a2fceac5af3aa
Lofimirai <permsalimanov2020@gmail.com>
2023-11-10 09:47:33 +0500

    Create script.js

---
+++ js/pr 8.3/js/script.js
@@ -0,0 +1,18 @@
+let arr={
+ sname:   "Ярков ",
+ name: "Дмитрий ",
+ lname: "Вительевич ",
+ ocua:  "Студент",
+ deg:   "Среднее",
+ categjri: "Первая",
+ edicushins:  "ОНИКС",
+ distanchins:  "Информатика",
+ primi4anie:  "Примечание",
+};
+document.write(arr.sname + arr.name + arr.lname +'<br>');
+document.write(arr.ocua +'<br>');
+document.write(arr.deg +'<br>');
+document.write(arr.categjri +'<br>');
+document.write(arr.edicushins +'<br>');
+document.write(arr.distanchins +'<br>');
+document.write(arr.primi4anie +'<br>');
