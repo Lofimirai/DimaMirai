3e917d8e971f8922c1707fc21ee712b10535e42d
Lofimirai <permsalimanov2020@gmail.com>
2023-11-17 09:42:17 +0500

    Update script.js

---
+++ js/pr 8.4/js/script.js
@@ -0,0 +1,22 @@
+let paragraph = document.querySelector("#myParagraph");
+let person={
+
+sname: 'Ярков',
+name : 'Дмитрий',
+postname : 'Витальевич',
+occu : 'Студент',
+deg : 'Среднее',
+categjri : 'Первая',
+edicushins : 'ГБПОУ ПКК ОНИКС',
+distanchins : 'Информатика',
+primi4anie : 'Примечание',
+
+};
+
+paragraph.innerHTML = "ФИО " + person.sname + ' ' + person.name + ' ' + person.postname +'<br>' 
++ "Должность " + person.occu +'<br>' 
++ "Уровень образования " + person.deg+'<br>' 
++ "Категория " + person.categjri +'<br>' 
++ "Образование " + person.edicushins +'<br>' 
++ "Дисциплина " + person.distanchins +'<br>' 
++ "Примечание " + person.primi4anie ;
